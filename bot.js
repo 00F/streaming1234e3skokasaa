81efad95ca8a3b0a6e46975de9962853976b8a3f
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-09 13:21:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`. FủLiĨ "#1813`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`~`,'https://www.twitch.tv/MeeRcY')
 client.user.setStatus("idle")
     
 });
