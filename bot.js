2704c1bebad99bccda79a4d4c25830c214d2de4d
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-09 14:37:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,36 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`~`,'https://www.twitch.tv/MeeRcY')
-client.user.setStatus("idle")
-    
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`. FủLiĨ "#1813`,'https://www.twitch.tv/MeeRcY')
+  client.user.setStatus("idle")
 });
-client.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('3Wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.send(`${argresult} \` `)
+} else 
+if (message.content.startsWith('2Ls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.send(` ${argresult} \` `)
+} else 
+if (message.content.startsWith('1St')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+   message.channel.send(`${argresult} \` `)
+}
+});
+
+client.on('message', message => {
+        if (!developers.includes(message.author.id)) return;
+  if (message.content === '712') {
+  let channel = client.channels.get('444873938691031040');
+
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
+    
+  }
+          
+});
